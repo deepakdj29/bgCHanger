3bccea250f0b80075e484204c1ef0ef3d82368b0
deepakdj29 <83852974+deepakdj29@users.noreply.github.com>
2024-03-06 12:33:52 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ const [color,setColor]= useState("olive")
   return (
    <>
    <div className="w-full h-screen duration-200" style={{ backgroundColor: color}}>
-    <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex felx-wrap justify-center gap-4 shadow-lg bg-white rounded-2xl px-3 py-2">
+    <div className=" fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className=" flex flex-wrap justify-center gap-4 shadow-lg bg-white rounded-2xl px-3 py-2">
             <button onClick={()=> setColor("red")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg " style={{ backgroundColor: "red"}}>Red</button>
             <button onClick={()=> setColor("blue")}
